a7689aff37411d0d6768b84e7abee344c7270787
Kemal Kocabıyık <kemal@ovidos.com>
2018-11-03 18:52:20 +0300

    updated assembly info.

--- CoinyPro.Client/Properties/AssemblyInfo.cs
+++ CoinyPro.Client/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Client Pro Client")]
-[assembly: AssemblyDescription("Client Pro Client")]
+[assembly: AssemblyTitle("Coiny Pro Client")]
+[assembly: AssemblyDescription("Coiny Pro Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Client Pro")]
-[assembly: AssemblyProduct("Client Pro Client")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("Client Pro")]
+[assembly: AssemblyCompany("Coiny Pro")]
+[assembly: AssemblyProduct("Coiny Pro Client")]
+[assembly: AssemblyCopyright("Copyright © Coiny 2018")]
+[assembly: AssemblyTrademark("Coiny Pro")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.6.0")]
-[assembly: AssemblyFileVersion("0.0.6.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
