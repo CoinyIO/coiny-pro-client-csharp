f7364c917ca7c0d2de060db30fa91460374cba7c
Kemal Kocabıyık <kemal@ovidos.com>
2019-04-05 14:04:28 +0300

    added extra properties to tradepair response.

--- CoinyPro.Client/Models/Responses/TradePairResponse.cs
+++ CoinyPro.Client/Models/Responses/TradePairResponse.cs
@@ -2,7 +2,11 @@
 {
     public class TradePairResponse
     {
+        public string Id { get; set; }
         public string Name { get; set; }
+        public double BaseMinSize { get; set; }
+        public double BaseMaxSize { get; set; }
+        public double QuoteIncrement { get; set; }
         public CurrencyResponse BaseCurrency { get; set; }
         public CurrencyResponse QuoteCurrency { get; set; }
     }
